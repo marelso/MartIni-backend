d1ed206f58309bb061046bf7b77b8c39fa57ea81
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-12-09 04:59:10 -0300

    Initial create with db and swagger

---
+++ src/main/java/com/marelso/martini/MartIniApplication.java
@@ -0,0 +1,13 @@
+package com.marelso.martini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartIniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartIniApplication.class, args);
+	}
+
+}
